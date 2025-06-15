64214d2a8539950a39c4aef03dd8dc8df61e59d3
{} <>
2025-06-15 12:09:49 +0100

    Feat: add address relocation

--- loader_stub/src/main.rs
+++ loader_stub/src/main.rs
@@ -1,4 +1,6 @@
-use goblin::pe::PE;
+use goblin::{elf::reloc, pe::{
+    self, data_directories, PE
+}};
 use windows::Win32::System::Memory::{
     VirtualAlloc, 
     VirtualProtect, 
@@ -21,6 +23,20 @@ unsafe fn alloc_exec_region(size: usize) -> *mut u8 {
     assert!(!alloc_address.is_null(), "VirtualAlloc failed!");
 
     return alloc_address as *mut u8;
+}
+
+fn rva_to_offset(pe: &PE, rva: usize) -> Option<usize> {
+    for section in &pe.sections {
+        let virtual_address = section.virtual_address as usize;
+        let virtual_size = section.virtual_size as usize;
+        let range = virtual_address..(virtual_address + virtual_size);
+        if range.contains(&rva) {
+            let delta = rva - virtual_address;
+            let file_off = section.pointer_to_raw_data as usize + delta;
+            return Some(file_off);
+        }
+    };
+    None
 }
 
 fn main() {
@@ -71,6 +87,43 @@ fn main() {
                 bss_size
             );
         }
+    }
+
+    let preferred_base = optional_header.windows_fields.image_base as usize;
+    let delta = (exec_region as usize) - preferred_base;
+
+    let base_relocation_table = optional_header.data_directories.get_base_relocation_table().expect("Failed to get base relocation table");
+    let reloc_virtual_address = base_relocation_table.virtual_address as usize;
+    let reloc_size = base_relocation_table.size as usize;
+    if (reloc_size == 0) ||(reloc_virtual_address == 0) {
+        println!("No relocation present.")
+    };
+    let mut offset = rva_to_offset(&pe, reloc_virtual_address).expect("RVA to offset failed");
+    let end = offset + reloc_size;
+
+    const IMAGE_REL_BASED_DIR64: u16 = 10;
+    while offset < end {
+        let page_rva = u32::from_le_bytes(PAYLOAD[offset..(offset + 4)].try_into().unwrap()) as usize;
+        let block_size = u32::from_le_bytes(PAYLOAD[(offset + 4)..(offset + 8)].try_into().unwrap()) as usize;
+        let entries = (block_size - 8) / 2;
+        let entries_ptr = PAYLOAD[(offset + 8)..].as_ptr() as *const u16;
+        
+        for i in 0..entries {
+            let entry_raw = unsafe { *entries_ptr.add(i) };
+            let entry_type = entry_raw >> 12;
+            let entry_offset = (entry_raw & 0x0FFF) as usize;
+
+            if entry_type == IMAGE_REL_BASED_DIR64 {
+                unsafe {
+                    let patch_ptr = exec_region.add(reloc_virtual_address + entry_offset) as *mut u64;
+                    let original_ptr = patch_ptr.read_unaligned();
+                    let correction = original_ptr + delta as u64;
+                    patch_ptr.write_unaligned(correction);
+                }
+            }
+        }
+
+        offset += block_size;
     }
 
 }
