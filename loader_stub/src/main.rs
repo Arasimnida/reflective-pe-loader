7edcd829638bad3751d8ec8b209bf8945ddb2b80
{} <>
2025-06-15 10:55:51 +0100

    Feat: parsing and copying headers

--- loader_stub/src/main.rs
+++ loader_stub/src/main.rs
@@ -1,9 +1,16 @@
-use windows::Win32::Foundation::HANDLE;
+use goblin::pe::PE;
 use windows::Win32::System::Memory::{
-    VirtualAlloc, VirtualProtect, MEM_COMMIT, MEM_RESERVE,
-    PAGE_EXECUTE_READ, PAGE_READWRITE,
+    VirtualAlloc, 
+    VirtualProtect, 
+    MEM_COMMIT, 
+    MEM_RESERVE,
+    PAGE_EXECUTE_READ, 
+    PAGE_READWRITE,
 };
 
+static PAYLOAD: &'static [u8] = include_bytes!("payload_messagebox.dll");
+
+#[allow(unsafe_op_in_unsafe_fn)]
 unsafe fn alloc_exec_region(size: usize) -> *mut u8 {
     let alloc_address = VirtualAlloc(
         None,
@@ -17,19 +24,17 @@ unsafe fn alloc_exec_region(size: usize) -> *mut u8 {
 }
 
 fn main() {
-    let size = 0x1000; // 4 KiB
-    let exec_region = unsafe { alloc_exec_region(size) };
-    println!("Allocated exec region at: {:p}", exec_region);
+    let pe = PE::parse(PAYLOAD).expect("Failed to parse DLL");
+    let optional_header = pe.header.optional_header.expect("Failed to get OptionalHeader");
+    //let standard_fields = optional_header.standard_fields;
+    let size_of_headers = optional_header.windows_fields.size_of_headers as usize;
+    let size_of_image = optional_header.windows_fields.size_of_image as usize;
 
+    println!("PE size_of_image = {:#x}", size_of_image);
+    println!("PE size_of_header = {:#x}", size_of_headers);
+    let exec_region = unsafe { alloc_exec_region(size_of_image) };
     unsafe {
-        let mut old = PAGE_READWRITE;
-        let success = VirtualProtect(
-            exec_region as *mut _,
-            size,
-            PAGE_EXECUTE_READ,
-            &mut old,
-        );
-        assert!(success.is_ok(), "VirtualProtect failed");
+        std::ptr::copy_nonoverlapping(PAYLOAD.as_ptr(), exec_region, size_of_headers);
     }
-    println!("Memory protection changed to RX.");
+    println!("Copied {} bytes of headers", size_of_headers);
 }
